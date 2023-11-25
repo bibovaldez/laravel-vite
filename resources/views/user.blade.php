74853ba211dfe2068c9979e02e42c5f7656a1b07
bibovaldez <bibovaldez2002@gmail.com>
2023-11-25 19:17:15 +0800

    none

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,13 +1,84 @@
 <!doctype html>
 <html>
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@500&family=Poppins:wght@800&display=swap"
+        rel="stylesheet" />
+    @vite('resources/css/app.css')
 </head>
+
 <body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-</body>
+
+    <body class="bg-brown-light">
+        <div class="container">
+            <div id="topbar" class="bg-green-dark box-content h-4 p-2 flex items-center justify-center space-x-2">
+                <svg class="w-7 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    stroke-width="1" stroke="white">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+                </svg>
+                <h1 class="text-white text-sm font-glacial font-medium">
+                    Smart energy management at your fingertips.
+                </h1>
+            </div>
+
+            <div id="navbar" class="flex items-center px-16 py-6 left-0">
+                <img src="../imgs/400164738_340907181921774_3802870888100654010_n-removebg (1).png" alt="logo"
+                    class="w-20" />
+                <div id="iKurtyente-Text">
+                    <h1 class="font-boldglacial font-bold text-green-light text-3xl ml-1 tracking-normal">
+                        iKuryente
+                    </h1>
+                </div>
+            </div>
+        </div>
+
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width=".5"
+            class="absolute top-0 right-0 mt-32 mr-96 stroke-current animate-updown w-14">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
+        </svg>
+
+        <div class="content-center text-center mt-5">
+            <div>
+                <h1 class="text-blue-light font-extrabold text-7xl font-league tracking-normal">
+                    The Smart Electric Meter
+                </h1>
+                <h2 class="text-green-dark font-extrabold text-6xl font-league tracking-normal -mt-11">
+                    for Power-saving companion
+                </h2>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width=".5"
+                    class="absolute bottom-0 left-0 stroke-current animate-updown ml-40 w-24 mb-36">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
+                </svg>
+
+                <p class="text-black-light px-80 font-glacial font-normal text-xl tracking-wider -mt-1 opacity-70">
+                    iKuryente is a web application designed for monitoring and controlling
+                    purposes, offering a streamlined and intuitive interface for
+                    effortless management.
+                </p>
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width=".5"
+                    class="absolute bottom-0 right-0 stroke-current animate-updown mr-48 mb-32 w-20">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
+                </svg>
+                <div id="Get-Started-Button" class="mt-9">
+                    <a href="login.html"
+                        class="text-center font-boldglacial font-bold text-xl tracking-wide items-center justify-center hover:opacity-80">
+                        <button class="text-white-light bg-green-dark rounded-md py-1 px-32 hover:underline">
+                            Get Started
+                        </button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </body>
+
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,108 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+module.exports = {
+    content: [
+        "./dist/*.{html,js}",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        colors: {
+            brown: {
+                light: "#FFFFFF",
+                DEFAULT: "#5c6ac4",
+                dark: "#FBF4E4",
+            },
+            green: {
+                dark: "#1BC037",
+                DEFAULT: "#1BC037",
+                light: "#87D75B",
+            },
+            white: {
+                DEFAULT: "#FFFFFF",
+                light: "#F7F8F7",
+            },
+            blue: {
+                light: "#26A2D9",
+                dark: "#C1D2F6",
+            },
+            black: {
+                light: "#2D2006",
+            },
+        },
+        fontFamily: {
+            glacial: ["Glacial Indifference", "sans-serif"],
+            boldglacial: ["Glacial Indifference Bold", "sans-serif"],
+            league: ["League Spartan", "sans-serif"],
+            openSans: ["Open Sans", "sans-serif"],
+        },
+        borderWidth: {
+            DEFAULT: "1px",
+            0: "0",
+            2: "2px",
+            3: "3px",
+        },
+        // create template for responsive font sizes
+        fontSize: {
+            xs: ".75rem",
+            sm: ".875rem",
+            tiny: ".875rem",
+            base: "1rem",
+            lg: "1.125rem",
+            xl: "1.25rem",
+            "2xl": "1.5rem",
+            "3xl": "2.0rem",
+            "4xl": "2.5rem",
+            "5xl": "3.0rem",
+            "6xl": "3.9rem",
+            "7xl": "5.5rem",
+            "8xl": "6.0rem",
+            "9xl": "7.0rem",
+            "10xl": "8.0rem",
+        },
+        // create template for font weights include font-thin, font-extrabold, etc.
+        fontWeight: {
+            thin: 100,
+            extralight: 200,
+            light: 300,
+            normal: 400,
+            medium: 500,
+            bold: 790,
+            extrabold: 800,
+            black: 900,
+        },
+        stroke: {
+            current: "#1BC037",
+        },
+        // create a template for up down animation
+        extend: {
+            keyframes: {
+                updown: {
+                    "0%, 100%": { transform: "translateY(0)" },
+                    "50%": { transform: "translateY(-30%)" },
+                },
+            },
+            animation: {
+                updown: "updown 4s ease-in-out infinite",
+            },
+        },
+        // create a template for opacity
+        opacity: {
+            0: "0",
+            10: ".1",
+            20: ".2",
+            30: ".3",
+            40: ".4",
+            50: ".5",
+            60: ".6",
+            70: ".84",
+            80: ".8",
+            90: ".9",
+            100: "1",
+        },
+    },
+    plugins: [],
+    variants: {
+        extend: {},
+    },
+};
